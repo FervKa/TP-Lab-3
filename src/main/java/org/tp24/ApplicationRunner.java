27ee2c70b08f9392127bfd013d23d4f4153f7a59
FervKa <ferbohi@outlook.com>
2024-09-18 21:15:12 -0500

    First solution

--- src/main/java/org/tp24/ApplicationRunner.java
+++ src/main/java/org/tp24/ApplicationRunner.java
@@ -3,16 +3,17 @@ package org.tp24;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.LocalDate;
+
 public class ApplicationRunner {
     protected static Logger logger = LogManager.getLogger();
-    Person person_class = new Person(10073823);
+    Person person_class = new Person(10073823, "Stiven", "Suarez", LocalDate.of(2000, 7, 23), "Cll 40#23AA-42", 500000);
 
     void run() {
         logger.info("The application is running!");
         logger.info(person_class.getPersonId());
+
+        person_class.printPerson();
     }
-
-
-
 
 }
--- src/main/java/org/tp24/Person.java
+++ src/main/java/org/tp24/Person.java
@@ -1,14 +1,81 @@
 package org.tp24;
 
+import java.time.LocalDate;
+
 public class Person {
 
-    private double personId;
+    private Double personId;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String address;
+    private Double maximumQuota;
 
-    public Person(double PersonId){
-        this.personId = PersonId;
+    public Person(double personId, String firstName, String lastName, LocalDate dateOfBirth, String address, double maximumQuota) {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.maximumQuota = maximumQuota;
     }
 
-    public int getPersonId(){
-        return (int)personId;
+    public Double getPersonId() {
+        return personId;
     }
+
+    public void setPersonId(Double personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return firstName;
+    }
+
+    public void setName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getMaximumQuota() {
+        return maximumQuota;
+    }
+
+    public void setMaximumBalance(Double maximumQuota) {
+        this.maximumQuota = maximumQuota;
+    }
+
+    public void printPerson() {
+        System.out.println("Person [ID: " + personId +
+                ", First Name: " + firstName +
+                ", Last Name: " + lastName +
+                ", Birth Date: " + dateOfBirth +
+                ", Address: " + address +
+                ", Salary: $" + maximumQuota + "]");
+    }
+
+    /*Something to push*/
 }
